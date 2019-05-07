2522a793646fe3d1a3eb7ffd4961b7e925d396f8
rou <rou@tangool-System-Product-Name>
2019-05-07 11:23:17 +0800

    Chapter 8 finished

---
+++ blog/article/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+from article.models import Article, Comment
+# Create your views here.
+def article(request):
+    '''
+    Render the article page
+    '''
+    articles = {}
+    for article in Article.objects.all():
+        articles.update({article:Comment.objects.filter(article=article)})
+    context = {'articles':articles}
+    return render(request, 'article/article.html', context)
